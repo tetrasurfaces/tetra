6b41dd959723b1f184141994401236cd1c6a57b4
tetrasurfaces <sendtotoddhutchinson@gmail.com>
2025-09-30 16:38:00 +0900

    Create id_util_tetra_surface.py

---
+++ id_util_tetra_surface.py
@@ -0,0 +1,82 @@
+# Copyright 2025 Todd Hutchinson, Anonymous
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# Proprietary Software - All Rights Reserved
+#
+# Copyright (C) 2025 Todd Hutchinson
+#
+# This software is proprietary and confidential. Unauthorized copying,
+# distribution, modification, or use is strictly prohibited without
+# express written permission from Todd Hutchinson.
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Define control points for a simple NURKS surface (e.g., a curved dome)
+control_points = np.array([
+    [[0, 0, 0], [1, 0, 0], [2, 0, 0]],
+    [[0, 1, 0], [1, 1, 2], [2, 1, 0]],
+    [[0, 2, 0], [1, 2, 0], [2, 2, 0]]
+])
+
+# Weights for rational aspect - expand to match shape
+weights = np.array([
+    [1, 1, 1],
+    [1, 1.5, 1],
+    [1, 1, 1]
+])
+
+# Kappa parameter for curvature adjustment
+kappa = 1.0  # Adjust this for more/less curvature
+
+# Simple NURBS surface evaluation (bilinear for demo)
+u = np.linspace(0, 1, 50)
+v = np.linspace(0, 1, 50)
+U, V = np.meshgrid(u, v)
+
+X = np.zeros_like(U)
+Y = np.zeros_like(U)
+Z = np.zeros_like(U)
+
+for i in range(len(u)):
+    for j in range(len(v)):
+        denom = 0
+        numer_x, numer_y, numer_z = 0, 0, 0
+        for a in range(3):
+            for b in range(3):
+                basis = (1 - u[i])**(2-a) * u[i]**a * (1 - v[j])**(2-b) * v[j]**b  # Bilinear basis (degree 2 approx)
+                w = weights[a, b] * basis
+                denom += w
+                numer_x += w * control_points[a, b, 0]
+                numer_y += w * control_points[a, b, 1]
+                numer_z += w * control_points[a, b, 2] * kappa  # Kappa modulates Z
+        X[i, j] = numer_x / denom
+        Y[i, j] = numer_y / denom
+        Z[i, j] = numer_z / denom
+
+# Plot the surface
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_wireframe(X, Y, Z, color='black')
+ax.plot_surface(X, Y, Z, cmap='viridis')
+
+# Show control points
+for a in range(3):
+    for b in range(3):
+        point = control_points[a, b]
+        ax.scatter(point[0], point[1], point[2], color='red', s=50)
+
+ax.set_title('NURKS Surface with Control Points')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+plt.show()
