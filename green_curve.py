e48312ca529e2c8e42357ddd6c35ee070981f29b
tetrasurfaces <sendtotoddhutchinson@gmail.com>
2025-10-09 06:42:53 +0900

    Update green_curve.py

--- green_curve.py
+++ green_curve.py
@@ -1,21 +1,26 @@
 # green_curve.py - B-Spline Curve Smoothing Function
 # Notes: Generates smoothed curves using B-spline interpolation with support for open and closed curves. Complete script; run as-is. Requires numpy (pip install numpy). Mentally verified: Open curve with 3 points → smooth interpolation; closed with 6 points → periodic loop.
 # Copyright 2025 Beau Ayres
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 # http://www.apache.org/licenses/LICENSE-2.0
-#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# Proprietary Software - All Rights Reserved
+#
+# This software is proprietary and confidential. Unauthorized copying,
+# distribution, modification, or use is strictly prohibited without
+# express written permission from Beau Ayres.
+#
+# AGPL-3.0-or-later licensed
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
