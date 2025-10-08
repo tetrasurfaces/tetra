f07a06e47cee6995972523173dd59a12d8254e48
tetrasurfaces <sendtotoddhutchinson@gmail.com>
2025-10-09 06:38:35 +0900

    Update ribit.py

--- ribit.py
+++ ribit.py
@@ -1,7 +1,35 @@
 # ribit.py - 7 Bit Ribit (Rainbow-Information-Bit) from Wise Transforms
-# SPDX-License-Identifier: AGPL-3.0-or-later
 # Notes: Generates a 7-bit ribit from BitWise, HexWise, HashWise braid. Maps to 7 logical states (0-6) with rainbow colors. Complete; run as-is. Mentally verified: Input='test' → Ribit=42 (example), State=0, Color=Red.
-
+# Copyright 2025 Beau Ayres
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# Proprietary Software - All Rights Reserved
+#
+# This software is proprietary and confidential. Unauthorized copying,
+# distribution, modification, or use is strictly prohibited without
+# express written permission from Beau Ayres.
+#
+# AGPL-3.0-or-later licensed
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 import hashlib
 import numpy as np
 import mpmath
