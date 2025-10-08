c77ab3bb573b1c4d721d122659121e77e6384603
tetrasurfaces <sendtotoddhutchinson@gmail.com>
2025-10-09 06:42:32 +0900

    Update gyro_gimbal.py

--- gyro_gimbal.py
+++ gyro_gimbal.py
@@ -29,7 +29,6 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 import numpy as np
 import mpmath
