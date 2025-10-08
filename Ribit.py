35fa92b9af9b617aef773e0429333ec19324f87f
tetrasurfaces <sendtotoddhutchinson@gmail.com>
2025-10-09 01:28:54 +0900

    Update Ribit.py

--- Ribit.py
+++ Ribit.py
@@ -1,57 +1,37 @@
-# Copyright Todd Hutchinson, Beau Ayres, Anonymous
+# Ribit.py
+# Copyright 2025 Beau Ayres
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 # http://www.apache.org/licenses/LICENSE-2.0
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# Proprietary Software - All Rights Reserved
 #
-# This software is proprietary and confidential. Unauthorized copying,
-# distribution, modification, or use is strictly prohibited without
-# express written permission from Todd Hutchinson and Beau Ayres.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import hashlib
 from PIL import Image
+from green_curve import bspline_basis, custom_interoperations_green_curve
 
-def bspline_basis(u, i, p, knots):
-    if p == 0:
-        return 1.0 if knots[i] <= u < knots[i + 1] else 0.0
-    term1 = ((u - knots[i]) / (knots[i + p] - knots[i])) * bspline_basis(u, i, p - 1, knots) if (knots[i + p] - knots[i]) > 0 else 0
-    term2 = ((knots[i + p + 1] - u) / (knots[i + p + 1] - knots[i + 1])) * bspline_basis(u, i + 1, p - 1, knots) if (knots[i + p + 1] - knots[i + 1]) > 0 else 0
-    return term1 + term2
-
-def custom_interoperations_green_curve(points, kappas, is_closed=False):
-    points = np.array(points)
-    kappas = np.array(kappas)
-    degree = 3
-    num_output_points = 1000
-    n = len(points)
-    if is_closed:
-        knots = np.concatenate((np.arange(-degree, 0), np.linspace(0, n, n - degree + 2), np.arange(n, n + degree)))
-    else:
-        knots = np.concatenate((np.zeros(degree + 1), np.linspace(0, 1, n - degree + 1)[1:-1], np.ones(degree + 1)))
-    u_fine = np.linspace(0, 1, num_output_points)
-    smooth_x = np.zeros(num_output_points)
-    smooth_y = np.zeros(num_output_points)
-    for j, u in enumerate(u_fine):
-        num_x, num_y, den = 0.0, 0.0, 0.0
-        for i in range(n):
-            b = bspline_basis(u, i, degree, knots)
-            w = kappas[i] * b
-            num_x += w * points[i, 0]
-            num_y += w * points[i, 1]
-            den += w
-        if den > 0:
-            smooth_x[j] = num_x / den
-            smooth_y[j] = num_y / den
-    return smooth_x, smooth_y
 
 def ribit_generate(data):
     ribit_hash = hashlib.sha256(data.encode()).digest()
