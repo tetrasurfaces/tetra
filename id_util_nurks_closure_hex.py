c2ca30511d38ff63282024ea75bd284b1c79f310
tetrasurfaces <sendtotoddhutchinson@gmail.com>
2025-10-09 06:40:57 +0900

    Update id_util_nurks_closure_hex.py

--- id_util_nurks_closure_hex.py
+++ id_util_nurks_closure_hex.py
@@ -1,4 +1,5 @@
-# Copyright 2025 Todd Hutchinson, Anonymous
+# id_util_nurks_closure_hex.py
+# Copyright 2025 Beau Ayres
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -10,11 +11,24 @@
 # limitations under the License.
 # Proprietary Software - All Rights Reserved
 #
-# Copyright (C) 2025 Todd Hutchinson
-#
 # This software is proprietary and confidential. Unauthorized copying,
 # distribution, modification, or use is strictly prohibited without
-# express written permission from Todd Hutchinson.
+# express written permission from Beau Ayres.
+#
+# AGPL-3.0-or-later licensed
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 import numpy as np
 import matplotlib.pyplot as plt
 
