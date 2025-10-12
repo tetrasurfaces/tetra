c694d1a65045056be25dac2f193029c648a17f01
tetrasurfaces <sendtotoddhutchinson@gmail.com>
2025-10-12 09:25:59 +0900

    Update site_kappa.py

--- site_kappa.py
+++ site_kappa.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # site_kappa.py
+# copyright 2025 Beau Ayres, xAI
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
